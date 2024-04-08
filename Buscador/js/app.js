8886d0582f237b9e4696aaf0744e0144ef81fe54
Mateo Gomez Sepulveda <143738590+MateoGomezSepulveda@users.noreply.github.com>
2024-04-08 14:39:32 -0500

    Creamos los selectores los cuales con un querySelector nos van a coger cada uno de los option que hay en nuestra pagina, Luego vamos a hacer los EventListener para nuestros filtros en el cual tenemos que coger a el valor de nuestros const parametros y dentro de el tambien vamos a inicializar una nueva funccion llamada filtrarCamper(); la cual nos va ayudar a filtrar cada uno de los option en el cual dentro de esta fuccion tambien vamos a inicializar una funccion por cada option la cual nos va ayudar a filtrar cada uno de ellos y por ultimo creamos nuestra funccion limpiar la cual vamos a hacer que elimine cada card dependiendo de el option que escogamos y la ponemos a ejecutar en nuestra linea 60 dentro de la funcion de showCampers.

--- Buscador/js/app.js
+++ Buscador/js/app.js
@@ -1,6 +1,17 @@
 console.log(campers);
 
 
+// Selectores
+
+const nombreCamper = document.querySelector('#nombre');
+const edadCamper = document.querySelector('#edad');
+const minPromedioCamper = document.querySelector('#minimo');
+const maxPromedioCamper = document.querySelector('#maximo');
+const nivelCampusCamper = document.querySelector('#nivelCampus');
+const nivelInglesCamper = document.querySelector('#nivelIngles')
+const especialidadCamper = document.querySelector('#especialidad')
+const expertoTecnologiaCamper = document.querySelector('#expertoTecnologia')
+
 
 
 // Inyectamos de forma dinamica los nombres y las edades
@@ -46,6 +57,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
 function showCampers(campers){
     const contenedorTarjetas = document.querySelector('#tarjetas')
+    limpiar()
     campers.forEach((camper) =>{
         const camperHTML = document.createElement('p');
         camperHTML.innerHTML = `
@@ -63,5 +75,131 @@ function showCampers(campers){
         contenedorTarjetas.appendChild(camperHTML);
     })
 }
+
+
+// EventListener de los filtros
+
+nombreCamper.addEventListener('input', (e)=>{
+    parametros.nombre = e.target.value
+    filtrarCamper();
+});
+
+edadCamper.addEventListener('input', e =>{
+    parametros.edad = Number(e.target.value)
+    filtrarCamper();
+});
+
+minPromedioCamper.addEventListener('input', (e)=>{
+    parametros.minPromedio = e.target.value
+    filtrarCamper();
+});
+
+maxPromedioCamper.addEventListener('input', (e)=>{
+    parametros.maxPromedio = e.target.value
+    filtrarCamper();
+});
+
+nivelCampusCamper.addEventListener('input', (e)=>{
+    parametros.nivelCampus = e.target.value
+    filtrarCamper();
+});
+
+nivelInglesCamper.addEventListener('input', (e)=>{
+    parametros.nivelIngles = e.target.value
+    filtrarCamper();
+});
+
+especialidadCamper.addEventListener('input', (e)=>{
+    parametros.especialidad = e.target.value
+    filtrarCamper();
+});
+
+expertoTecnologiaCamper.addEventListener('input', (e)=>{
+    parametros.expertoTecnologia = e.target.value
+    filtrarCamper();
+});
+
+
+
+
+
+// Declaracion de funcion
+function filtrarCamper (){
+    const resultado = campers
+    .filter(filtrarNombre)
+    .filter(filtrarEdad)
+    .filter(filtrarMinPromedio)
+    .filter(filtrarMaxPromedio)
+    .filter(filtrarNivelCampus)
+    .filter(filtrarNivelIngles)
+    .filter(filtrarEspecialidad)
+    .filter(filtrarExpertoTecnologia)
+    console.log(resultado);
+    showCampers(resultado)
+}
+
+function filtrarNombre (camper){
+    if (parametros.nombre){
+        return camper.nombre === parametros.nombre
+    }
+    return camper
+}
+
+function filtrarEdad (camper){
+    if (parametros.edad){
+        return camper.edad === parametros.edad
+    }
+    return camper
+}
+
+function filtrarMinPromedio (camper){
+    if (parametros.minPromedio){
+        return camper.promedio >= parametros.minPromedio
+    }
+    return camper
+}
+
+function filtrarMaxPromedio (camper){
+    if (parametros.maxPromedio){
+        return camper.promedio >= parametros.maxPromedio
+    }
+    return camper
+}
+
+function filtrarNivelCampus (camper){
+    if (parametros.nivelCampus){
+        return camper.nivelCampus === parametros.nivelCampus
+    }
+    return camper
+}
+
+function filtrarNivelIngles (camper){
+    if (parametros.nivelIngles){
+        return camper.nivelIngles === parametros.nivelIngles
+    }
+    return camper
+}
+
+function filtrarEspecialidad (camper){
+    if (parametros.especialidad){
+        return camper.especialidad === parametros.especialidad
+    }
+    return camper
+}
+
+function filtrarExpertoTecnologia (camper){
+    if (parametros.expertoTecnologia){
+        return camper.expertoTecnologia === parametros.expertoTecnologia
+    }
+    return camper
+}
+
+function limpiar(){
+    let m=document.querySelectorAll('.card')
+    for(let a=0; a<m.length; a++){
+        m[a].remove()
+    }
+}
+
 
 
