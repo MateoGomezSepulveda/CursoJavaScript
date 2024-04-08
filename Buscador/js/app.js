3ad54b861781f89055851b660561fdbe8ab05119
Mateo Gomez Sepulveda <143738590+MateoGomezSepulveda@users.noreply.github.com>
2024-04-08 14:12:14 -0500

    en este punto vamos a crear un objeto de nombre paramentros para nuestro criterios para la busqueda, lego vamos a crear nuestro DOMContentLoaded y en ella vamos a poner a crear nuestra funcion de showCampers dentro de esta funcion vamos a seleccionar nuestro div tarjetas y en el vamos a ingresar nuestras cards de forma dinamica

--- Buscador/js/app.js
+++ Buscador/js/app.js
@@ -1,6 +1,8 @@
 console.log(campers);
 
 
+
+
 // Inyectamos de forma dinamica los nombres y las edades
 
 campers.forEach((optionCamper)=>{
@@ -18,6 +20,48 @@ for(let i = min; i < max; i++){
     option.value = i;
     option.textContent = i;
     document.querySelector('#edad').appendChild(option)
+}
+
+
+// Objeto con parametros o criterios para busqueda
+
+const parametros = {
+    nombre: "",
+    edad: "",
+    minPromedio: "",
+    maxPromedio: "",
+    nivelCampus: "",
+    nivelIngles: "",
+    especialidad: "",
+    expertoTecnologia: ""
+}
+
+
+// EventListener
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    showCampers(campers);
+    console.log(parametros);
+})
+
+function showCampers(campers){
+    const contenedorTarjetas = document.querySelector('#tarjetas')
+    campers.forEach((camper) =>{
+        const camperHTML = document.createElement('p');
+        camperHTML.innerHTML = `
+        <div class="card" style="width: 18rem;">
+            <img src="img/${camper.imagen}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${camper.nombre}</h5>
+              <p class="card-text">${camper.detalle}</p>
+            </div>
+            <ul class="list-group list-group-flush">
+              <a href="#" class="btn btn-danger"> Details </a>
+            </ul>
+          </div>
+        `;
+        contenedorTarjetas.appendChild(camperHTML);
+    })
 }
 
 
